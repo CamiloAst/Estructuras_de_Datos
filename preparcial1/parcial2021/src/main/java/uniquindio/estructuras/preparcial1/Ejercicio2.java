774264ce8bf06407536eaf38f0a7cee691a95fc7
Juan Camilo Astudillo <juanc.astudilloo@uqvirtual.edu.co>
2023-09-11 21:23:54 -0500

    Creación de clases
    Se crearon las clases para los ejercicios 1  2

---
+++ preparcial1/parcial2021/src/main/java/uniquindio/estructuras/preparcial1/Ejercicio2.java
@@ -0,0 +1,13 @@
+package uniquindio.estructuras.preparcial1;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String [] arreglo = {"A E D R T Y U",
+                             "F Y U I H V G",
+                             "A Y U I H N M",
+                             "I P U I H O L"};
+        int i=0,j=0;
+//        arreglo[i][j]
+        System.out.println(arreglo[i].charAt(j));
+    }
+}
