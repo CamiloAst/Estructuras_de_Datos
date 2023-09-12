a6af5394f73b4ac6d4f5ac9e722486930335c214
Juan Camilo Astudillo <juanc.astudilloo@uqvirtual.edu.co>
2023-09-12 14:13:11 -0500

    Ejercicio 3
    Se terminó el punto 3 del primer parcial

--- preparcial1/parcial2021/src/main/java/uniquindio/estructuras/preparcial1/Ejercicio3.java
+++ preparcial1/parcial2021/src/main/java/uniquindio/estructuras/preparcial1/Ejercicio3.java
@@ -1,5 +1,7 @@
 package uniquindio.estructuras.preparcial1;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,12 +9,21 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         List<Integer> numeros = new ArrayList<>();
         numeros.add(0);
+        numeros.add(2);
+        numeros.add(3);
         int n = compararNumeros(numeros);
         System.out.println(n);
     }
 
-    private static int compararNumeros(List<Integer> numeros) {
-        return (numeros.get(numeros.size() - 1) > numeros.size() ? -1 : 1);
+    private static int compararNumeros(@NotNull List<Integer> numeros) {
+        if(numeros.size() <= 1)
+            return numeros.get(0);
+        else{
+            numeros.remove(0);
+            if(compararNumeros(numeros)==numeros.size())
+                return 0;
+            return (compararNumeros(numeros)>numeros.size() ? 1 : -1);
+        }
     }
 
 }
